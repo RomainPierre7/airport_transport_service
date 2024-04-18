738c1d11d238b6123f5c2527049f3dfc2d727a4f
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2024-04-18 14:04:17 +0300

    Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.js';
 import About from './pages/About.js';
 import Reserve from './pages/Reserve';
 import Contact from './pages/Contact.js';
 import Profile from './pages/Profile.js';
-import './assets/App.css';
 import IsAuthenticated from './utils/IsAuthenticated';
 import LoginPage from './pages/LoginPage';
 import Navbar from './components/Navabar.js';
-import HomeOne from './components/HomeOne.js';
-import HomeTwo from './components/HomeTwo.js';
-import HomeThree from './components/HomeThree.js';
-import HomeFour from './components/HomeFour.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/reserve" element={<Reserve />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/" element={<HomeOne />} />
-        <Route path="/" element={<HomeTwo />} />
-        <Route path="/" element={<HomeThree />} />
-        <Route path="/" element={<HomeFour />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,17 @@
+import HomeOne from '../components/HomeOne.js';
+import HomeTwo from '../components/HomeTwo.js';
+import HomeThree from '../components/HomeThree.js';
+import HomeFour from '../components/HomeFour.js';
+
+function Home() {
+    return (
+        <div>
+            <HomeOne />
+            <HomeTwo />
+            <HomeThree />
+            <HomeFour />
+        </div>
+    );
+}
+
+export default Home;
