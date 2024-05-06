5c49b714138aa4dd266581dd20116b6a6c400dd1
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2024-05-06 21:27:04 +0300

    ticket editing

--- backend/controllers/reservationController.js
+++ backend/controllers/reservationController.js
@@ -33,12 +33,56 @@ exports.getReservationByID = asyncHandler(async (req, res, next) => {
 
 exports.getTicketByReservationID = asyncHandler(async (req, res, next) => {
     const reservationID = req.params.reservationID;
+    const login = req.user.login;
 
-    generatePdf.generatePDF(reservationID, 1, 'Paris', 'Lyon', '2021-06-10', 'John', 'Doe', 50)
-        .then(() => {
-            res.setHeader('Content-Type', 'application/pdf');
-            res.sendFile(path.join(__dirname, `../pdf/ticket_${reservationID}.pdf`));
-        })
+    db.query('SELECT T.TRIPMAINDIRECTION, R.TRIPID, S.STOPNAME, S.STOPID, C.CUSTOMERNAME, C.CUSTOMERSURNAME, R.RESERVATIONPRICE FROM RESERVATIONS R JOIN CUSTOMERS C ON R.CUSTOMERID = C.CUSTOMERID JOIN STOPS S ON S.STOPID = R.STOPID JOIN TRIPS T ON T.TRIPID = R.TRIPID WHERE RESERVATIONID = ?', [reservationID], (err, rows) => {
+        if (err) {
+            console.error('Error executing query', err.stack);
+            return res.status(500).send('Error executing query');
+        }
+        if (rows.length === 0) {
+            return res.status(404).send('Reservation not found or not yours');
+        }
+        const tripMainDirection = rows[0].TRIPMAINDIRECTION;
+        const tripID = rows[0].TRIPID;
+        const stop = rows[0].STOPNAME;
+        const stopID = rows[0].STOPID;
+        const name = rows[0].CUSTOMERNAME;
+        const surname = rows[0].CUSTOMERSURNAME;
+        const price = rows[0].RESERVATIONPRICE;
+
+        if (tripMainDirection === 1) {
+            db.query('SELECT (SELECT STOPNAME FROM STOPS WHERE STOPID = 1) AS AIRPORTNAME, SCHEDULETIME FROM SCHEDULES WHERE STOPID = ? AND TRIPID = ?', [stopID, tripID], (err, rows) => {
+                if (err) {
+                    console.error('Error executing query', err.stack);
+                    return res.status(500).send('Error executing query');
+                }
+                const airportName = rows[0].AIRPORTNAME;
+                const scheduleTime = rows[0].SCHEDULETIME;
+
+                generatePdf.generatePDF(reservationID, tripID, stop, airportName, new Date(scheduleTime).toLocaleString(), name, surname, price)
+                    .then(() => {
+                        res.setHeader('Content-Type', 'application/pdf');
+                        res.sendFile(path.join(__dirname, `../pdf/ticket_${reservationID}.pdf`));
+                    })
+            });
+        } else {
+            db.query('SELECT (SELECT STOPNAME FROM STOPS WHERE STOPID = 1) AS AIRPORTNAME, SCHEDULETIME FROM SCHEDULES WHERE STOPID = 1 AND TRIPID = ?', [tripID], (err, rows) => {
+                if (err) {
+                    console.error('Error executing query', err.stack);
+                    return res.status(500).send('Error executing query');
+                }
+                const airportName = rows[0].AIRPORTNAME;
+                const scheduleTime = rows[0].SCHEDULETIME;
+
+                generatePdf.generatePDF(reservationID, tripID, airportName, stop, new Date(scheduleTime).toLocaleString(), name, surname, price)
+                    .then(() => {
+                        res.setHeader('Content-Type', 'application/pdf');
+                        res.sendFile(path.join(__dirname, `../pdf/ticket_${reservationID}.pdf`));
+                    })
+            });
+        }
+    });
 });
 
 exports.createReservation = asyncHandler(async (req, res, next) => {
