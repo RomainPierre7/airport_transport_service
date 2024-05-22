368a02d5d8351a94ef38b822bc966d5e397cb743
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2024-05-22 23:53:59 +0300

    css button

--- frontend/src/components/general/Navbar.js
+++ frontend/src/components/general/Navbar.js
@@ -25,39 +25,39 @@ function Navbar() {
 
   return (
     <div className='Anav'>
-      <nav className="navbar navbar-expand-lg"  style={{margin:'auto'}}>
+      <nav className="navbar navbar-expand-lg" style={{ margin: 'auto' }}>
         <div className="container-fluid">
           <Link className="navbar-brand" to="/"><img src={Logo} alt="Logo" />SkyRide</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav" style={{textAlign:'center'}}>
-          <ul className="navbar-nav mb-5 mb-lg-0 m-flex">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/reserve">Reserve</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Contact</Link>
-            </li>
-          </ul>
+          <div className="collapse navbar-collapse" id="navbarNav" style={{ textAlign: 'center' }}>
+            <ul className="navbar-nav mb-5 mb-lg-0 m-flex">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/reserve">Reserve</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Contact</Link>
+              </li>
+            </ul>
           </div>
-          <div className="d-flex"  id="navbarNav" style={{textAlign:'center'}}>
-          {!isLoggedIn && (
-            <Link className="nav-link" to="signup_login">
-              <button type="button" className="btn btn-light">Sign Up / Log in</button>
-            </Link>)
-          }
-          {isLoggedIn && (
-            <Link className="nav-link" to="account">
-              <button type="button" className="btn btn-light">My account</button>
-            </Link>)
-          }
-          {isLoggedIn && (
-            <button type="button" className="btn btn-light" onClick={handleLogout}>Log out</button>
-          )}
+          <div className="d-flex" id="navbarNav" style={{ textAlign: 'center' }}>
+            {!isLoggedIn && (
+              <Link className="nav-link" to="signup_login">
+                <button type="button" className="btn btn-light">Sign Up / Log in</button>
+              </Link>)
+            }
+            {isLoggedIn && (
+              <Link className="nav-link" to="account">
+                <button type="button" className="btn btn-light" style={{ textAlign: 'center', margin: '0 20px' }}>My account</button>
+              </Link>)
+            }
+            {isLoggedIn && (
+              <button type="button" className="btn btn-light" onClick={handleLogout}>Log out</button>
+            )}
           </div>
         </div>
       </nav>
